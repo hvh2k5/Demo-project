56197873a7199a524708b2355d719b6eb70d6173
ngo-duy <166359669+ngo-duy@users.noreply.github.com>
2024-04-17 13:08:47 +0700

    Update trangsanpham.php

--- Project1/trangsanpham.php
+++ Project1/trangsanpham.php
@@ -19,8 +19,6 @@ $laptopgaming = mysqli_query($conn, $sql);
 
 
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,27 +28,68 @@ $laptopgaming = mysqli_query($conn, $sql);
     <title>LaptopGaming</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <style>
+        .product-card {
+            background-color: #999999;
+            border: 1px solid #ddd;
+            border-radius: 30px;
+            padding: 20px;
+            margin-bottom: 20px;
+            display: flex;
+            /* Sử dụng flexbox */
+            flex-direction: column;
+            /* Thiết lập hướng của các phần tử con */
+            align-items: center;
+            /* Căn giữa theo chiều ngang */
+        }
+
+        .product-image {
+            width: 100%;
+            height: auto;
+            margin-bottom: 15px;
+            /* Khoảng cách giữa ảnh và các phần tử khác */
+        }
+
+        .product-name {
+            font-size: 18px;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        .product-price {
+            font-size: 16px;
+            color: red;
+            font-weight: bold;
+            margin-bottom: auto;
+            /* Đẩy giá tiền xuống cuối cùng của product-card */
+        }
+    </style>
 </head>
 
 <body>
     <div class="container">
-        <h1 style="text-align: center;" >Trang bán LAPTOPGAMING</h1>
+        <h1 style="text-align: center;">Trang bán LaptopGaming</h1>
+        <form>
+            <input class="form-control mb-5" name="search" placeholder="Nhập sản phẩm muốn tìm" />
+        </form>
         <div class="row row-cols-3">
-
             <?php
-        while ($row = mysqli_fetch_array($laptopgaming)){
-            echo"<div class='col'>";
+            while ($row = mysqli_fetch_array($laptopgaming)) {
+                ?>
+                <div class="col">
+                    <div class="product-card">
+                        <img class="product-image" src="<?php echo $row['image']; ?>" alt="Product Image">
+                        <p class="product-name"><?php echo $row['name']; ?></p>
+                        <p class="product-price">Giá bán: <?php echo $row['price']; ?></p>
+                        <a href="chitietgaming.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Chi tiết</a>
+                    </div>
+                </div>
 
-            echo"<p>".$row['name']."</p>";
-            echo"<img class='img-fluid' src='".$row['image']."'/>";
-            echo"<p>".$row['price']."</p>";
-            echo"</div>";
-        }
+                <?php
+            }
             ?>
         </div>
-
-
-
     </div>
 </body>
 
